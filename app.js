8280fb87dc0a514b433ac1f4bd40031af872fe8a
Saqib Khan <saaqibkhann8@gmail.com>
2025-11-19 21:52:25 +0500

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+function menu(){
+    let pages = document.getElementById('pages');
+
+    if(pages.style.display === "block"){
+        pages.style.display = "none";
+    }else{
+        pages.style.display = "block"
+    }
+}
